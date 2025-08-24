00a4e4f68da9452d8c4a03d11907dab7259ca782
EnderKill98 <no@mail.tld>
2025-08-24 03:53:39 +0200

    Initial and final commit

---
+++ src/main/java/me/enderkill98/fix2b2tghostitems/mixin/NoUnstackableDragMixin.java
@@ -0,0 +1,23 @@
+package me.enderkill98.fix2b2tghostitems.mixin;
+
+import net.minecraft.client.gui.screen.ingame.HandledScreen;
+import net.minecraft.screen.ScreenHandler;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(HandledScreen.class)
+public class NoUnstackableDragMixin<T extends ScreenHandler> {
+
+    @Shadow @Final protected T handler;
+
+    @Inject(method = "mouseDragged", at = @At("HEAD"), cancellable = true)
+    public void mouseDragged(CallbackInfoReturnable<Boolean> cir) {
+        if(!handler.getCursorStack().isEmpty() && !handler.getCursorStack().isStackable())
+            cir.setReturnValue(true); // Prevent initiating a drag, if item is unstackable
+    }
+
+}
